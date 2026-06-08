56ee208cf3ebc3cf398f31438ad07000c1dd1650
thonglo-app <porda80@gmail.com>
2026-06-08 16:37:50 +0800

    Fix Vercel routing: rewrite /api/* to serverless function, let Vite serve static files

--- api/index.js
+++ api/index.js
@@ -1,7 +1,3 @@
-const path = require('path');
-const express = require('express');
 const app = require('../server');
-
-app.use(express.static(path.join(__dirname, '..', 'dist')));
 
 module.exports = app;
