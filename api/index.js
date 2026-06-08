2afb1019ed8aefcbfee1ac39429f6635608f0a45
thonglo-app <porda80@gmail.com>
2026-06-08 16:34:07 +0800

    Fix: serve Vite-built dist/index.html via express.static for root route

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
+const path = require('path');
+const express = require('express');
 const app = require('../server');
+
+app.use(express.static(path.join(__dirname, '..', 'dist')));
 
 module.exports = app;
