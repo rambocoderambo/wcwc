a6bcc85a32d1d5dae8bc3860154478baa70cb533
thonglo-app <porda80@gmail.com>
2026-06-08 16:31:58 +0800

    Fix: simplify api/index.js - let Vercel handle static files natively

--- api/index.js
+++ api/index.js
@@ -1,20 +1,3 @@
-const path = require('path');
-const express = require('express');
-const fs = require('fs');
 const app = require('../server');
-
-const root = path.join(__dirname, '..');
-app.use(express.static(root));
-
-app.get('*', (req, res, next) => {
-  if (req.path.startsWith('/api/')) return next();
-  const html = path.join(root, 'index.html');
-  if (fs.existsSync(html)) {
-    res.set('Content-Type', 'text/html; charset=utf-8');
-    res.sendFile(html);
-  } else {
-    res.status(404).send('Not found');
-  }
-});
 
 module.exports = app;
